eb27e6e4c1d195ad527b94cf6303ef66efeb7c9e
Hadarmalul <hm6267@bs.amalnet.k12i.il>
2021-03-13 23:15:37 +0200

    ...

--- app/src/main/java/com/example/beta/FBref 2.java
+++
@@ -1,9 +0,0 @@
-package com.example.beta;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-public class FBref {
-
-    public static FirebaseAuth mAuth=FirebaseAuth.getInstance();
-}
